913653c3fcc32db09ff55161614fa3f8178c0812
trungdang <dientrung.py3@gmail.com>
2021-10-26 02:06:12 +0700

    Init commit

---
+++ book_store/checkout/paypal.py
@@ -0,0 +1,11 @@
+import sys
+
+from paypalcheckoutsdk.core import PayPalHttpClient, SandboxEnvironment
+
+
+# class PayPalClient:
+#     def __init__(self):
+#         self.client_id = "AXh8m1Y--QeLcp2ibW0S_uWjTkc8FAeHjtrrYjeFBKY5e0NamSkNkE0R"
+#         self.client_secret = ""
+#         self.environment = SandboxEnvironment(client_id=self.client_id, client_secret=self.client_secret)
+#         self.client = PayPalHttpClient(self.environment)
